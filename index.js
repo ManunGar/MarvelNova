b685977290f2ced5e201855b42d2ba6462aebdd3
MarvelNova <86012117+MarvelNova@users.noreply.github.com>
2021-06-16 18:05:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle("show");
+});
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".menu-btn").classList.toggle("rot");
+});
